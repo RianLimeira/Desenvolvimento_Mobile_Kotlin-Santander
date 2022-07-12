f90253c95a56c9d5eba67d042bbd771ca70b6d47
RianLimeira <rianlimeira@gmail.com>
2022-07-12 20:31:03 -0300

    create class

---
+++ fundamentosOO/src/main/kotlin/BancoTest.kt
@@ -0,0 +1,5 @@
+fun main() {
+    val digiOneBank = Banco("DigiOne", 10)
+    println(digiOneBank.nome)
+    println(digiOneBank.numero)
+}
---
+++ fundamentosOO/src/main/kotlin/Conta.kt
@@ -0,0 +1,14 @@
+import java.math.BigDecimal
+
+class Conta(
+    val agencia:String,
+    val numero:String,
+    val saldo: BigDecimal
+) {
+    fun depoisico(valor: BigDecimal){
+
+    }
+    fun saque(valor:BigDecimal){
+
+    }
+}
