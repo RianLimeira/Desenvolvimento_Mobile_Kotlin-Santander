124030bade52f8a192070fe66d5ca2711a03ef7b
RianLimeira <rianlimeira@gmail.com>
2022-07-21 13:42:28 -0300

    update class operacoes

--- api-collections/collections-api/src/main/kotlin/one/digitalinnovation/collections/TestOperacoes.kt
+++ api-collections/collections-api/src/main/kotlin/one/digitalinnovation/collections/TestOperacoes.kt
@@ -7,6 +7,7 @@ fun main(){
     for (salario in salarios) {
         println(salario)
     }
+
     println("*".repeat(10))
     println("Numero de salarios: " + salarios.count())
     println("Maior salario: ${salarios.maxOrNull()}")
@@ -14,7 +15,22 @@ fun main(){
     println("Menor salario: ${salarios.minOrNull()}")
     println("Média salario: ${salarios.average()}")
 
+    //filtro
     val salariosMaior2500 = salarios.filter { it > 2000.0 }
     println("*".repeat(10))
     salariosMaior2500.forEach{ println(it) }
+    println("*".repeat(10))
+
+    //contador
+    println(salarios.count({it in 2000.0 .. 5000.0}))
+    println("*".repeat(10))
+
+    //busca pelo valor (exemplo uma conta de banco), retorna valor or null
+    println(salarios.find { it == 2250.0 })
+    println(salarios.find { it == 50.0 })
+    println("*".repeat(10))
+
+    //busca qualquer valor valido, retorna true/false
+    println(salarios.any({it == 1000.0}))
+    println(salarios.any({it == 50.0}))
 }
