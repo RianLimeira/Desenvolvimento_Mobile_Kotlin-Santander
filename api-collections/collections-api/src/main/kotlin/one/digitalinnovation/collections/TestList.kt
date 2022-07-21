7461deaf3c61d74052d6fb2e45a9709513291eed
RianLimeira <rianlimeira@gmail.com>
2022-07-21 14:17:10 -0300

    update sortedBy

--- api-collections/collections-api/src/main/kotlin/one/digitalinnovation/collections/TestList.kt
+++ api-collections/collections-api/src/main/kotlin/one/digitalinnovation/collections/TestList.kt
@@ -1,17 +1,18 @@
 package one.digitalinnovation.collections
 
 fun main(){
-    val rian = Funcionario("Rian", 1000.0)
-    val gabriely = Funcionario ("Gabriely", 2000.0)
+    val rian = Funcionario("Rian", 1500.0)
+    val gabriely = Funcionario ("Gabriely", 3000.0)
     val daniel = Funcionario ("Daniel", 2500.0)
 
     val funcionarios = listOf(rian, gabriely, daniel)
 
-    funcionarios.forEach{ println(it)
+    funcionarios.forEach{ println(it) }
     println("*".repeat(10))
-    }
-    
-    println(funcionarios.find { it.nome == "gabriely" })
+    println(funcionarios.find { it.nome == "Gabriely" })
+    println("*".repeat(10))
+
+    funcionarios.sortedBy { it.salario }.forEach { println(it) }
 }
 
 data class Funcionario(
